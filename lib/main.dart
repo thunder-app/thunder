eaab2fb404a27d399eb1ed46b6fc08d24b5f3492
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-06-17 00:33:21 -0700

    ignoring certain high count sentry events

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // External Packages
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:path/path.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
-import 'package:sqflite/sqflite.dart';
 
 // Internal Packages
 import 'package:thunder/routes.dart';
 import 'package:thunder/core/enums/theme_type.dart';
 import 'package:thunder/thunder/bloc/thunder_bloc.dart';
 import 'package:thunder/core/auth/bloc/auth_bloc.dart';
+
+// Ignore specific exceptions to send to Sentry
+FutureOr<SentryEvent?> beforeSend(SentryEvent event, {Hint? hint}) async {
+  if (event.exceptions != null &&
+      event.exceptions!.isNotEmpty &&
+      event.exceptions!.first.value != null &&
+      event.exceptions!.first.value!.contains('The request returned an invalid status code of 400.')) {
+    return null;
+  }
+
+  return event;
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,7 +39,8 @@ void main() async {
       (options) {
         options.dsn = kDebugMode ? '' : sentryDSN;
         options.debug = kDebugMode;
-        options.tracesSampleRate = 1.0;
+        options.tracesSampleRate = 0.7;
+        options.beforeSend = beforeSend;
       },
       appRunner: () => runApp(const ThunderApp()),
     );
