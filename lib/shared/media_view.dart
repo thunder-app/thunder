9d35e787fd7ad133cd0f35563633d6b31dde9303
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-06-19 00:42:44 -0700

    added ability to see fullscreen image when tapping on image in feed or post

--- lib/shared/media_view.dart
+++ lib/shared/media_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:thunder/core/enums/media_type.dart';
 import 'package:thunder/core/models/post_view_media.dart';
+import 'package:thunder/shared/image_viewer.dart';
 import 'package:thunder/shared/link_preview_card.dart';
 
 import 'package:url_launcher/url_launcher.dart';
@@ -38,69 +39,74 @@ class MediaView extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(6),
-        child: Stack(
-          children: [
-            CachedNetworkImage(
-              imageUrl: postView!.media.first.mediaUrl!,
-              height: showFullHeightImages ? postView!.media.first.height : 150,
-              width: postView!.media.first.width ?? MediaQuery.of(context).size.width - 24,
-              fit: BoxFit.fitWidth,
-              progressIndicatorBuilder: (context, url, downloadProgress) => Container(
-                color: Colors.grey.shade900,
-                child: Center(
-                  child: SizedBox(
-                    width: 40,
-                    height: 40,
-                    child: CircularProgressIndicator(value: downloadProgress.progress),
+      child: GestureDetector(
+        onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => ImageViewer(url: postView!.media.first.mediaUrl!),
+        )),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(6),
+          child: Stack(
+            children: [
+              CachedNetworkImage(
+                imageUrl: postView!.media.first.mediaUrl!,
+                height: showFullHeightImages ? postView!.media.first.height : 150,
+                width: postView!.media.first.width ?? MediaQuery.of(context).size.width - 24,
+                fit: BoxFit.fitWidth,
+                progressIndicatorBuilder: (context, url, downloadProgress) => Container(
+                  color: Colors.grey.shade900,
+                  child: Center(
+                    child: SizedBox(
+                      width: 40,
+                      height: 40,
+                      child: CircularProgressIndicator(value: downloadProgress.progress),
+                    ),
                   ),
                 ),
-              ),
-              errorWidget: (context, url, error) => Container(
-                color: Colors.grey.shade900,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
-                  child: InkWell(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(6), // Image border
-                      child: Stack(
-                        alignment: Alignment.bottomRight,
-                        fit: StackFit.passthrough,
-                        children: [
-                          Container(
-                            color: Colors.grey.shade900,
-                            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
-                            child: Row(
-                              children: [
-                                const Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                                  child: Icon(
-                                    Icons.link,
-                                    color: Colors.white60,
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Text(
-                                    post?.url ?? '',
-                                    overflow: TextOverflow.ellipsis,
-                                    style: theme.textTheme.bodyMedium!.copyWith(
+                errorWidget: (context, url, error) => Container(
+                  color: Colors.grey.shade900,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 4.0, bottom: 8.0),
+                    child: InkWell(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6), // Image border
+                        child: Stack(
+                          alignment: Alignment.bottomRight,
+                          fit: StackFit.passthrough,
+                          children: [
+                            Container(
+                              color: Colors.grey.shade900,
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
+                              child: Row(
+                                children: [
+                                  const Padding(
+                                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                    child: Icon(
+                                      Icons.link,
                                       color: Colors.white60,
                                     ),
                                   ),
-                                ),
-                              ],
+                                  Expanded(
+                                    child: Text(
+                                      post?.url ?? '',
+                                      overflow: TextOverflow.ellipsis,
+                                      style: theme.textTheme.bodyMedium!.copyWith(
+                                        color: Colors.white60,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
+                      onTap: () => _launchURL(post?.url!),
                     ),
-                    onTap: () => _launchURL(post?.url!),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
