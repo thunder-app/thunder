fb3d9776510ad360a149f09657a194d89fa58ccf
guigs44 <guigs45@outlook.com>
2023-06-20 14:09:52 -0300

    Added the toggle for hiding nsfw post thumbnails
    Default option: Hide

--- lib/post/widgets/post_view.dart
+++ lib/post/widgets/post_view.dart
@@ -24,6 +24,8 @@ class PostSubview extends StatelessWidget {
     final theme = Theme.of(context);
 
     final bool isUserLoggedIn = context.read<AuthBloc>().state.isLoggedIn;
+    final bool hideNsfwPreviews = context.read<ThunderBloc>().state.preferences?.getBool('setting_general_show_save_action') ?? true;
+
 
     return Padding(
       padding: const EdgeInsets.only(left: 12.0, right: 12.0, bottom: 8.0),
@@ -77,7 +79,7 @@ class PostSubview extends StatelessWidget {
               ),
             ],
           ),
-          MediaView(post: postView.post, postView: postView),
+          MediaView(post: postView.post, postView: postView, hideNsfwPreviews: hideNsfwPreviews,),
           if (postView.post.body != null)
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
