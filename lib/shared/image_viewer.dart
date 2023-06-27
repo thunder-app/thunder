f5910ae9ddedee3ed802981274df6103fe829ca8
Anton <mrantonsaenko@gmail.com>
2023-06-27 09:59:50 -0400

    Reimplemented download function

--- lib/shared/image_viewer.dart
+++ lib/shared/image_viewer.dart
@@ -1,8 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:extended_image/extended_image.dart';
 
 import 'package:thunder/shared/hero.dart';
+
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+import 'package:permission_handler/permission_handler.dart';
 
 class ImageViewer extends StatefulWidget {
   final String url;
@@ -14,51 +25,90 @@ class ImageViewer extends StatefulWidget {
 }
 
 class _ImageViewerState extends State<ImageViewer> {
-  GlobalKey<ExtendedImageSlidePageState> slidePagekey = GlobalKey<ExtendedImageSlidePageState>();
+  GlobalKey<ExtendedImageSlidePageState> slidePagekey =
+      GlobalKey<ExtendedImageSlidePageState>();
+
+  _requestPermission() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.photos,
+    ].request();
+
+    final info2 = statuses[Permission.photos].toString();
+    print(info2);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       backgroundColor: Colors.black87,
-      body: Center(
-        child: ExtendedImageSlidePage(
-          key: slidePagekey,
-          slideAxis: SlideAxis.both,
-          slideType: SlideType.onlyImage,
-          child: GestureDetector(
-            child: HeroWidget(
-              tag: widget.url,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: IconButton(
+              color: theme.textTheme.titleLarge?.color,
+              onPressed: () async {
+                File file = await DefaultCacheManager().getSingleFile(widget.url);
+                if ((Platform.isAndroid || Platform.isIOS) &&
+                    await Permission.contacts.request().isGranted) {
+                  final result = await ImageGallerySaver.saveFile(file.path);
+                } else if (Platform.isLinux || Platform.isWindows) {
+                  final filePath =
+                      '${(await getApplicationDocumentsDirectory()).path}/ThunderImages/${basename(file.path)}';
+                  File(filePath)
+                    ..createSync(recursive: true)
+                    ..writeAsBytesSync(file.readAsBytesSync());
+                }
+              },
+              icon: const Icon(
+                Icons.download,
+                semanticLabel: 'Download',
+              ),
+            ),
+          ),
+          Center(
+            child: ExtendedImageSlidePage(
+              key: slidePagekey,
+              slideAxis: SlideAxis.both,
               slideType: SlideType.onlyImage,
-              slidePagekey: slidePagekey,
-              child: ExtendedImage.network(
-                widget.url,
-                enableSlideOutPage: true,
-                mode: ExtendedImageMode.gesture,
-                cache: true,
-                clearMemoryCacheWhenDispose: true,
-                initGestureConfigHandler: (ExtendedImageState state) {
-                  return GestureConfig(
-                    minScale: 0.9,
-                    animationMinScale: 0.7,
-                    maxScale: 4.0,
-                    animationMaxScale: 4.5,
-                    speed: 1.0,
-                    inertialSpeed: 100.0,
-                    initialScale: 1.0,
-                    inPageView: false,
-                    initialAlignment: InitialAlignment.center,
-                    reverseMousePointerScrollDirection: true,
-                    gestureDetailsIsChanged: (GestureDetails? details) {},
-                  );
+              child: GestureDetector(
+                child: HeroWidget(
+                  tag: widget.url,
+                  slideType: SlideType.onlyImage,
+                  slidePagekey: slidePagekey,
+                  child: ExtendedImage.network(
+                    widget.url,
+                    enableSlideOutPage: true,
+                    mode: ExtendedImageMode.gesture,
+                    cache: true,
+                    clearMemoryCacheWhenDispose: true,
+                    initGestureConfigHandler: (ExtendedImageState state) {
+                      return GestureConfig(
+                        minScale: 0.9,
+                        animationMinScale: 0.7,
+                        maxScale: 4.0,
+                        animationMaxScale: 4.5,
+                        speed: 1.0,
+                        inertialSpeed: 100.0,
+                        initialScale: 1.0,
+                        inPageView: false,
+                        initialAlignment: InitialAlignment.center,
+                        reverseMousePointerScrollDirection: true,
+                        gestureDetailsIsChanged: (GestureDetails? details) {},
+                      );
+                    },
+                  ),
+                ),
+                onTap: () {
+                  slidePagekey.currentState!.popPage();
+                  Navigator.pop(context);
                 },
               ),
             ),
-            onTap: () {
-              slidePagekey.currentState!.popPage();
-              Navigator.pop(context);
-            },
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,6 +7,7 @@
 #include "generated_plugin_registrant.h"
 
 #include <dynamic_color/dynamic_color_plugin_c_api.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <sentry_flutter/sentry_flutter_plugin.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
@@ -14,6 +15,8 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DynamicColorPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DynamicColorPluginCApi"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   SentryFlutterPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
