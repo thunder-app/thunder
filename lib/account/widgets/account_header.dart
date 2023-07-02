b9cb05b8d0647dcab571c2fed02d3d0c344d30b5
dbuckles <108231591+bactaholic@users.noreply.github.com>
2023-07-01 19:40:01 -0700

    readded comment/post counter
    The original alpha had a comment/post counter, I added this back to the profile and added a setting to toggle counter or score.
    
    Since this is on a per-app, per-user basis and its not visible to anyone else it shouldn't promote karma farming.

--- lib/account/widgets/account_header.dart
+++ lib/account/widgets/account_header.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:intl/intl.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -8,7 +11,10 @@ import 'package:lemmy_api_client/v3.dart';
 import 'package:thunder/shared/icon_text.dart';
 import 'package:thunder/utils/instance.dart';
 
+import 'package:thunder/thunder/bloc/thunder_bloc.dart';
+
 class AccountHeader extends StatelessWidget {
+
   final PersonViewSafe? accountInfo;
 
   const AccountHeader({super.key, this.accountInfo});
@@ -19,6 +25,9 @@ class AccountHeader extends StatelessWidget {
 
     final DateFormat formatter = DateFormat('yyyy-MM-dd');
     final String formatted = formatter.format(accountInfo!.person.published);
+
+    final bool profileSettings = context.read<ThunderBloc>().state.preferences?.getBool('setting_general_hide_profile_score') ?? true;
+    final totalScore = accountInfo!.counts.commentScore + accountInfo!.counts.postScore;
 
     return Padding(
       padding: const EdgeInsets.only(top: 8.0, left: 24.0, right: 24.0, bottom: 24.0),
@@ -57,6 +66,26 @@ class AccountHeader extends StatelessWidget {
                         text: (accountInfo?.person.published != null) ? '${formatted}' : '-',
                       ),
                     ],
+                  ),
+                  Row(
+                    children: [
+                    if (profileSettings == false)(
+                      IconText(
+                          icon: const Icon(
+                            Icons.arrow_upward_rounded,
+                            size: 16,
+                          ),
+                          text: '${(accountInfo?.counts.postScore).toString()} · ${(accountInfo?.counts.commentScore).toString()} · ${(totalScore).toString()}')
+                  )else if (profileSettings == true)(
+                        IconText(
+                          icon: const Icon(
+                            Icons.note_rounded,
+                            size:16,
+                          ),
+                          text: '${(accountInfo?.counts.postCount).toString()} · ${(accountInfo?.counts.commentCount)}'
+                        )
+                      )
+                    ]
                   ),
                 ],
               ),
