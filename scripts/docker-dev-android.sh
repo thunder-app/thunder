5be8ffbac0110dbf92d6680cddfefb65b5f79876
Fmstrat <nospam@nowsci.com>
2023-07-03 19:29:27 -0400

    volume mount for docker development

--- scripts/docker-dev-android.sh
+++ scripts/docker-dev-android.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker run --rm -ti --net host --name thunder-dev thunder-builder bash -c 'flutter devices && flutter run'
+docker run --rm -ti --net host -v "${SCRIPT_DIR}/../lib:/build/lib" --name thunder-dev thunder-builder bash -c 'flutter devices && flutter run'
