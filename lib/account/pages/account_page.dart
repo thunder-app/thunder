eb096496737c2af81d32013e248112a8dc8de8e2
db <108231591+bactaholic@users.noreply.github.com>
2023-07-05 17:57:45 -0700

    reverting account_page.dart
    reverting to match develop branch to avoid merge conflict

--- lib/account/pages/account_page.dart
+++ lib/account/pages/account_page.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:thunder/account/bloc/account_bloc.dart';
-import 'package:thunder/account/pages/account_page_success.dart';
-import 'package:thunder/account/widgets/profile_modal_body.dart';
+import 'package:thunder/account/utils/profiles.dart';
 import 'package:thunder/core/auth/bloc/auth_bloc.dart';
-import 'package:thunder/thunder/bloc/thunder_bloc.dart';
+import 'package:thunder/user/pages/user_page.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -23,92 +22,31 @@ class _AccountPageState extends State<AccountPage> {
     AuthState authState = context.read<AuthBloc>().state;
     AccountState accountState = context.read<AccountBloc>().state;
 
-    return Scaffold(
-      appBar: AppBar(
-        scrolledUnderElevation: 0,
-        leading: authState.isLoggedIn
-            ? IconButton(
-                onPressed: () => context.read<AuthBloc>().add(RemoveAccount(accountId: context.read<AuthBloc>().state.account!.id)),
-                icon: const Icon(
-                  Icons.logout,
-                  semanticLabel: 'Log out',
-                ),
-              )
-            : null,
-        actions: [
-          if (authState.isLoggedIn)
-            Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: IconButton(
-                  onPressed: () => context.read<AccountBloc>().add(const GetAccountContent(reset: true)),
-                  icon: const Icon(
-                    Icons.refresh_rounded,
-                    semanticLabel: 'Refresh',
-                    ),
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        if (authState.isLoggedIn && accountState.status == AccountStatus.success) return UserPage(userId: accountState.personView!.person.id, isAccountUser: true);
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.people_rounded, size: 100, color: theme.dividerColor),
+                const SizedBox(height: 16),
+                const Text('Add an account to see your profile', textAlign: TextAlign.center),
+                const SizedBox(height: 24.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(60)),
+                  child: const Text('Manage Accounts'),
+                  onPressed: () => showProfileModalSheet(context),
                 )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: IconButton(
-                onPressed: () => showProfileModalSheet(context),
-                icon: const Icon(
-                  Icons.people_alt_rounded,
-                  semanticLabel: 'Profiles',
-                ),
-              ),
-            ),
-        ],
-      ),
-      body: BlocConsumer<AuthBloc, AuthState>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: authState.isLoggedIn && accountState.status == AccountStatus.success
-                  ? AccountPageSuccess(accountState: accountState)
-                  : Center(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.people_rounded, size: 100, color: theme.dividerColor),
-                          const SizedBox(height: 16),
-                          const Text('Add an account to see your profile', textAlign: TextAlign.center),
-                          const SizedBox(height: 24.0),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(60)),
-                            child: const Text('Manage Accounts'),
-                            onPressed: () => showProfileModalSheet(context),
-                          )
-                        ],
-                      ),
-                    ),
-            ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
-  }
-
-  void showProfileModalSheet(BuildContext context) {
-    AuthBloc authBloc = context.read<AuthBloc>();
-    ThunderBloc thunderBloc = context.read<ThunderBloc>();
-    showModalBottomSheet(
-        isScrollControlled: true,
-        context: context,
-        showDragHandle: true,
-        builder: (context) {
-          return MultiBlocProvider(
-            providers: [
-              BlocProvider.value(value: authBloc),
-              BlocProvider.value(value: thunderBloc),
-            ],
-            child: const FractionallySizedBox(
-              heightFactor: 0.9,
-              child: ProfileModalBody(),
-            ),
-          );
-        });
   }
 }
