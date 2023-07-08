af1c870c55181540d280603d3ab2d964d7a45bc9
bactaholic <social@dbuckles.me>
2023-07-07 18:39:24 -0700

    added confirmation to close app via OS back button

--- lib/thunder/pages/thunder_page.dart
+++ lib/thunder/pages/thunder_page.dart
@@ -112,74 +112,101 @@ class _ThunderState extends State<Thunder> {
             case ThunderStatus.success:
               return Scaffold(
                 bottomNavigationBar: _getScaffoldBottomNavigationBar(context),
-                body: MultiBlocProvider(
-                  providers: [
-                    BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
-                    BlocProvider<AccountBloc>(create: (context) => AccountBloc()),
-                  ],
-                  child: BlocConsumer<AuthBloc, AuthState>(
-                    listenWhen: (AuthState previous, AuthState current) {
-                      if (previous.isLoggedIn != current.isLoggedIn || previous.status == AuthStatus.initial) return true;
-                      return false;
+                body: WillPopScope(
+                    onWillPop: () async {
+                  bool shouldThunderClose = await showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                        title: Text('Confirm Close'),
+                        content: Text('Are you sure you want to exit thunder?'),
+                        actions: [
+                    TextButton(
+                    child: Text('No'),
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
                     },
-                    listener: (context, state) {
-                      context.read<AccountBloc>().add(GetAccountInformation());
-                      context.read<InboxBloc>().add(const GetInboxEvent());
-                    },
-                    builder: (context, state) {
-                      switch (state.status) {
-                        case AuthStatus.initial:
-                          context.read<AuthBloc>().add(CheckAuth());
-                          return const Center(child: CircularProgressIndicator());
-                        case AuthStatus.loading:
-                          WidgetsBinding.instance.addPostFrameCallback((_) => setState(() => selectedPageIndex = 0));
-                          return const Center(child: CircularProgressIndicator());
-                        case AuthStatus.success:
-                          Version? version = thunderBlocState.version;
-                          bool showInAppUpdateNotification = thunderBlocState.showInAppUpdateNotification;
-                          bool? enableSentryErrorTracking = thunderBlocState.enableSentryErrorTracking;
-                          if (version?.hasUpdate == true && hasShownUpdateDialog == false && showInAppUpdateNotification == true) {
-                            WidgetsBinding.instance.addPostFrameCallback((_) {
-                              showUpdateNotification(context, version);
 
-                              setState(() => hasShownUpdateDialog = true);
-                            });
+                    ),
+                          TextButton(
+                            child: Text('Yes'),
+                            onPressed: () {
+                              Navigator.of(context).pop(true);
+                              Navigator.of(context).pop(true); //2nd pop here is to force exit when sidebar is open
+                            },
+                          ),
+                        ]
+                    )
+                  );
+                  return shouldThunderClose ?? false;
+                },
+                    child: MultiBlocProvider(
+                      providers: [
+                        BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
+                        BlocProvider<AccountBloc>(create: (context) => AccountBloc()),
+                      ],
+                      child: BlocConsumer<AuthBloc, AuthState>(
+                        listenWhen: (AuthState previous, AuthState current) {
+                          if (previous.isLoggedIn != current.isLoggedIn || previous.status == AuthStatus.initial) return true;
+                          return false;
+                        },
+                        listener: (context, state) {
+                          context.read<AccountBloc>().add(GetAccountInformation());
+                          context.read<InboxBloc>().add(const GetInboxEvent());
+                        },
+                        builder: (context, state) {
+                          switch (state.status) {
+                            case AuthStatus.initial:
+                              context.read<AuthBloc>().add(CheckAuth());
+                              return const Center(child: CircularProgressIndicator());
+                            case AuthStatus.loading:
+                              WidgetsBinding.instance.addPostFrameCallback((_) => setState(() => selectedPageIndex = 0));
+                              return const Center(child: CircularProgressIndicator());
+                            case AuthStatus.success:
+                              Version? version = thunderBlocState.version;
+                              bool showInAppUpdateNotification = thunderBlocState.showInAppUpdateNotification;
+                              bool? enableSentryErrorTracking = thunderBlocState.enableSentryErrorTracking;
+                              if (version?.hasUpdate == true && hasShownUpdateDialog == false && showInAppUpdateNotification == true) {
+                                WidgetsBinding.instance.addPostFrameCallback((_) {
+                                  showUpdateNotification(context, version);
+
+                                  setState(() => hasShownUpdateDialog = true);
+                                });
+                              }
+
+                              // Ask user if they want to opt-in to Sentry for the first time (based on if setting_error_tracking_enable_sentry is null)
+                              if (enableSentryErrorTracking == null && hasShownSentryDialog == false) {
+                                WidgetsBinding.instance.addPostFrameCallback((_) {
+                                  showSentryNotification(context);
+                                  setState(() => hasShownSentryDialog = true);
+                                });
+                              }
+
+                              return PageView(
+                                controller: pageController,
+                                onPageChanged: (index) => setState(() => selectedPageIndex = index),
+                                physics: const NeverScrollableScrollPhysics(),
+                                children: <Widget>[
+                                  CommunityPage(scaffoldKey: _feedScaffoldKey),
+                                  BlocProvider(
+                                    create: (context) => SearchBloc(),
+                                    child: const SearchPage(),
+                                  ),
+                                  const AccountPage(),
+                                  const InboxPage(),
+                                  SettingsPage(),
+                                ],
+                              );
+                            case AuthStatus.failure:
+                              return ErrorMessage(
+                                message: state.errorMessage,
+                                action: () => {context.read<AuthBloc>().add(CheckAuth())},
+                                actionText: 'Refresh Content',
+                              );
                           }
-
-                          // Ask user if they want to opt-in to Sentry for the first time (based on if setting_error_tracking_enable_sentry is null)
-                          if (enableSentryErrorTracking == null && hasShownSentryDialog == false) {
-                            WidgetsBinding.instance.addPostFrameCallback((_) {
-                              showSentryNotification(context);
-                              setState(() => hasShownSentryDialog = true);
-                            });
-                          }
-
-                          return PageView(
-                            controller: pageController,
-                            onPageChanged: (index) => setState(() => selectedPageIndex = index),
-                            physics: const NeverScrollableScrollPhysics(),
-                            children: <Widget>[
-                              CommunityPage(scaffoldKey: _feedScaffoldKey),
-                              BlocProvider(
-                                create: (context) => SearchBloc(),
-                                child: const SearchPage(),
-                              ),
-                              const AccountPage(),
-                              const InboxPage(),
-                              SettingsPage(),
-                            ],
-                          );
-                        case AuthStatus.failure:
-                          return ErrorMessage(
-                            message: state.errorMessage,
-                            action: () => {context.read<AuthBloc>().add(CheckAuth())},
-                            actionText: 'Refresh Content',
-                          );
-                      }
-                    },
-                  ),
-                ),
-              );
+                        },
+                      ),
+                    )
+              ));
             case ThunderStatus.failure:
               return ErrorMessage(
                 message: thunderBlocState.errorMessage,
