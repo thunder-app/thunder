1196069e72eb1111e5dad8af4f660d2ff54b9984
CTalvio <ctalvio@gmail.com>
2023-07-09 03:20:35 +0300

    Added community icons to subscribed list and search

--- lib/search/pages/search_page.dart
+++ lib/search/pages/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -139,6 +140,11 @@ class _SearchPageState extends State<SearchPage> {
             CommunityView communityView = state.results!.communities[index];
 
             return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: communityView.community.icon != null ? Colors.transparent : theme.colorScheme.onBackground,
+                  foregroundImage: communityView.community.icon != null ? CachedNetworkImageProvider(communityView.community.icon!) : null,
+                  maxRadius: 25,
+                ),
                 title: Text(
                   communityView.community.title,
                   overflow: TextOverflow.ellipsis,
