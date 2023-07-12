f8bb7cd47f95c43176115087592dd3cb1e6111f6
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-07-12 15:34:17 -0700

    added ability to dismiss keyboard when tapping outside of text field in create comment modal

--- lib/post/widgets/create_comment_modal.dart
+++ lib/post/widgets/create_comment_modal.dart
@@ -141,159 +141,164 @@ class _CreateCommentModalState extends State<CreateCommentModal> {
             },
           ),
       ],
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('${widget.isEdit ? 'Edit' : 'Create'} Comment', style: theme.textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold)),
-                  IconButton(
-                    onPressed: (isSubmitButtonDisabled || isLoading)
-                        ? null
-                        : () {
-                            if (widget.isEdit) {
-                              return context.read<PostBloc>().add(EditCommentEvent(content: _bodyTextController.text, commentId: widget.commentView!.comment!.comment.id));
-                            }
+      child: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('${widget.isEdit ? 'Edit' : 'Create'} Comment', style: theme.textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold)),
+                    IconButton(
+                      onPressed: (isSubmitButtonDisabled || isLoading)
+                          ? null
+                          : () {
+                              if (widget.isEdit) {
+                                return context.read<PostBloc>().add(EditCommentEvent(content: _bodyTextController.text, commentId: widget.commentView!.comment!.comment.id));
+                              }
 
-                            if (widget.comment != null) {
-                              context.read<InboxBloc>().add(CreateInboxCommentReplyEvent(content: _bodyTextController.text, parentCommentId: widget.comment!.id, postId: widget.comment!.postId));
-                            } else {
-                              context.read<PostBloc>().add(CreateCommentEvent(content: _bodyTextController.text, parentCommentId: widget.commentView?.comment!.comment.id));
-                            }
-                          },
-                    icon: isLoading
-                        ? const SizedBox(width: 20, height: 20, child: CircularProgressIndicator())
-                        : const Icon(
-                            Icons.send_rounded,
-                            semanticLabel: 'Reply',
-                          ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 12.0),
-              if (widget.commentView != null && widget.isEdit == false)
-                Text('Replying to ${widget.commentView?.comment!.creator.name ?? 'N/A'}', style: theme.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w400)),
-              if (widget.comment != null && widget.isEdit == false)
-                Text('Replying to ${widget.parentCommentAuthor ?? 'N/A'}', style: theme.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w400)),
+                              if (widget.comment != null) {
+                                context.read<InboxBloc>().add(CreateInboxCommentReplyEvent(content: _bodyTextController.text, parentCommentId: widget.comment!.id, postId: widget.comment!.postId));
+                              } else {
+                                context.read<PostBloc>().add(CreateCommentEvent(content: _bodyTextController.text, parentCommentId: widget.commentView?.comment!.comment.id));
+                              }
+                            },
+                      icon: isLoading
+                          ? const SizedBox(width: 20, height: 20, child: CircularProgressIndicator())
+                          : const Icon(
+                              Icons.send_rounded,
+                              semanticLabel: 'Reply',
+                            ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12.0),
+                if (widget.commentView != null && widget.isEdit == false)
+                  Text('Replying to ${widget.commentView?.comment!.creator.name ?? 'N/A'}', style: theme.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w400)),
+                if (widget.comment != null && widget.isEdit == false)
+                  Text('Replying to ${widget.parentCommentAuthor ?? 'N/A'}', style: theme.textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w400)),
 
-              if ((widget.commentView != null || widget.comment != null) && widget.isEdit == false)
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-                  margin: const EdgeInsets.symmetric(vertical: 8.0),
-                  constraints: const BoxConstraints(maxHeight: 150.0),
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(border: Border.all(color: theme.dividerColor), borderRadius: BorderRadius.circular(8.0), color: theme.cardColor),
-                  child: Scrollbar(
-                    controller: _scrollController,
-                    trackVisibility: true,
-                    radius: const Radius.circular(16.0),
-                    child: SingleChildScrollView(
+                if ((widget.commentView != null || widget.comment != null) && widget.isEdit == false)
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+                    margin: const EdgeInsets.symmetric(vertical: 8.0),
+                    constraints: const BoxConstraints(maxHeight: 150.0),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(border: Border.all(color: theme.dividerColor), borderRadius: BorderRadius.circular(8.0), color: theme.cardColor),
+                    child: Scrollbar(
                       controller: _scrollController,
-                      child: CommonMarkdownBody(
-                        body: widget.commentView != null ? (widget.commentView?.comment?.comment.content ?? 'N/A') : (widget.comment?.content ?? 'N/A'),
-                        isSelectableText: true,
+                      trackVisibility: true,
+                      radius: const Radius.circular(16.0),
+                      child: SingleChildScrollView(
+                        controller: _scrollController,
+                        child: CommonMarkdownBody(
+                          body: widget.commentView != null ? (widget.commentView?.comment?.comment.content ?? 'N/A') : (widget.comment?.content ?? 'N/A'),
+                          isSelectableText: true,
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-              // Text(
-              //   fetchInstanceNameFromUrl(widget.communityInfo?.communityView.community.actorId) ?? 'N/A',
-              //   style: theme.textTheme.titleMedium?.copyWith(
-              //     color: theme.textTheme.titleMedium?.color?.withOpacity(0.7),
-              //     fontWeight: FontWeight.w400,
-              //   ),
-              // ),
+                // Text(
+                //   fetchInstanceNameFromUrl(widget.communityInfo?.communityView.community.actorId) ?? 'N/A',
+                //   style: theme.textTheme.titleMedium?.copyWith(
+                //     color: theme.textTheme.titleMedium?.color?.withOpacity(0.7),
+                //     fontWeight: FontWeight.w400,
+                //   ),
+                // ),
 
-              const SizedBox(height: 12.0),
-              // Center(
-              //   child: ToggleButtons(
-              //     direction: Axis.horizontal,
-              //     onPressed: (int index) {
-              //       setState(() {
-              //         // The button that is tapped is set to true, and the others to false.
-              //         for (int i = 0; i < _selectedPostType.length; i++) {
-              //           _selectedPostType[i] = i == index;
-              //         }
-              //       });
-              //     },
-              //     borderRadius: const BorderRadius.all(Radius.circular(8)),
-              //     constraints: BoxConstraints.expand(width: (MediaQuery.of(context).size.width / postTypes.length) - 12.0),
-              //     isSelected: _selectedPostType,
-              //     children: postTypes,
-              //   ),
-              // ),
-              ListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                children: <Widget>[
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 200,
-                        child: (showPreview)
-                            ? Container(
-                                decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(10)),
-                                padding: const EdgeInsets.all(12),
-                                child: SingleChildScrollView(
-                                  child: CommonMarkdownBody(body: description),
+                const SizedBox(height: 12.0),
+                // Center(
+                //   child: ToggleButtons(
+                //     direction: Axis.horizontal,
+                //     onPressed: (int index) {
+                //       setState(() {
+                //         // The button that is tapped is set to true, and the others to false.
+                //         for (int i = 0; i < _selectedPostType.length; i++) {
+                //           _selectedPostType[i] = i == index;
+                //         }
+                //       });
+                //     },
+                //     borderRadius: const BorderRadius.all(Radius.circular(8)),
+                //     constraints: BoxConstraints.expand(width: (MediaQuery.of(context).size.width / postTypes.length) - 12.0),
+                //     isSelected: _selectedPostType,
+                //     children: postTypes,
+                //   ),
+                // ),
+                ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: <Widget>[
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 200,
+                          child: (showPreview)
+                              ? Container(
+                                  decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(10)),
+                                  padding: const EdgeInsets.all(12),
+                                  child: SingleChildScrollView(
+                                    child: CommonMarkdownBody(body: description),
+                                  ),
+                                )
+                              : MarkdownTextInput(
+                                  (String value) => setState(() => description = value),
+                                  description,
+                                  label: 'Comment',
+                                  maxLines: 5,
+                                  actions: const [
+                                    MarkdownType.link,
+                                    MarkdownType.bold,
+                                    MarkdownType.italic,
+                                    MarkdownType.blockquote,
+                                    MarkdownType.strikethrough,
+                                    MarkdownType.title,
+                                    MarkdownType.list,
+                                    MarkdownType.separator,
+                                    MarkdownType.code,
+                                  ],
+                                  controller: _bodyTextController,
+                                  textStyle: theme.textTheme.bodyLarge,
+                                  // textStyle: const TextStyle(fontSize: 16),
                                 ),
-                              )
-                            : MarkdownTextInput(
-                                (String value) => setState(() => description = value),
-                                description,
-                                label: 'Comment',
-                                maxLines: 5,
-                                actions: const [
-                                  MarkdownType.link,
-                                  MarkdownType.bold,
-                                  MarkdownType.italic,
-                                  MarkdownType.blockquote,
-                                  MarkdownType.strikethrough,
-                                  MarkdownType.title,
-                                  MarkdownType.list,
-                                  MarkdownType.separator,
-                                  MarkdownType.code,
-                                ],
-                                controller: _bodyTextController,
-                                textStyle: theme.textTheme.bodyLarge,
-                                // textStyle: const TextStyle(fontSize: 16),
-                              ),
-                      ),
-                      const SizedBox(height: 8.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          TextButton(
-                            onPressed: isClearButtonDisabled
-                                ? null
-                                : () {
-                                    _bodyTextController.clear();
-                                    setState(() => showPreview = false);
-                                  },
-                            child: const Text('Clear'),
-                          ),
-                          TextButton(
-                            onPressed: () => setState(() => showPreview = !showPreview),
-                            child: Text(showPreview == true ? 'Show Markdown' : 'Show Preview'),
-                          ),
-                        ],
-                      ),
-                      if (isFailure) AutoSizeText(errorMessage ?? 'Error', maxLines: 3)
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                        ),
+                        const SizedBox(height: 8.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              onPressed: isClearButtonDisabled
+                                  ? null
+                                  : () {
+                                      _bodyTextController.clear();
+                                      setState(() => showPreview = false);
+                                    },
+                              child: const Text('Clear'),
+                            ),
+                            TextButton(
+                              onPressed: () => setState(() => showPreview = !showPreview),
+                              child: Text(showPreview == true ? 'Show Markdown' : 'Show Preview'),
+                            ),
+                          ],
+                        ),
+                        if (isFailure) AutoSizeText(errorMessage ?? 'Error', maxLines: 3)
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
