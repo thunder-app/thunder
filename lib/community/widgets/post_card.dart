880a331d50ad5591077af77ee90a0f86c0b17cbe
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-07-13 14:51:45 -0700

    fixed issue where disabling one side of the post swipe gestures would also disable the comment gesture on that side even if there were gestures assigned to the comments

--- lib/community/widgets/post_card.dart
+++ lib/community/widgets/post_card.dart
@@ -101,7 +101,7 @@ class _PostCardState extends State<PostCard> {
         },
         onPointerCancel: (event) => {},
         child: Dismissible(
-          direction: state.enablePostGestures == false ? DismissDirection.none : determineSwipeDirection(isUserLoggedIn, state),
+          direction: state.enablePostGestures == false ? DismissDirection.none : determinePostSwipeDirection(isUserLoggedIn, state),
           key: ObjectKey(widget.postViewMedia.postView.post.id),
           resizeDuration: Duration.zero,
           dismissThresholds: const {DismissDirection.endToStart: 1, DismissDirection.startToEnd: 1},
