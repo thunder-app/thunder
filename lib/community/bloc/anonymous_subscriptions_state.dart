136454b8056ef4661cf6069ce8bc57013a42665a
Bindu <code@bindu.io>
2023-07-14 07:56:51 -0700

    Add anonymous subscriptions bloc and allow to subscribe to communities from search page

---
+++ lib/community/bloc/anonymous_subscriptions_state.dart
@@ -0,0 +1,35 @@
+part of 'anonymous_subscriptions_bloc.dart';
+
+enum AnonymousSubscriptionsStatus { initial, loading, refreshing, success, empty, failure }
+
+class AnonymousSubscriptionsState extends Equatable {
+  const AnonymousSubscriptionsState({
+    this.status = AnonymousSubscriptionsStatus.initial,
+    this.subscriptions = const [],
+    this.ids = const {},
+    this.errorMessage,
+  });
+
+  final AnonymousSubscriptionsStatus status;
+  final String? errorMessage;
+  final List<CommunitySafe> subscriptions;
+  final Set<int> ids;
+
+
+  AnonymousSubscriptionsState copyWith({
+    AnonymousSubscriptionsStatus? status,
+    List<CommunitySafe>? subscriptions,
+    Set<int>? ids,
+    String? errorMessage,
+  }) {
+    return AnonymousSubscriptionsState(
+      status: status ?? this.status,
+      ids: ids?? this.ids,
+      subscriptions: subscriptions ?? this.subscriptions,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, subscriptions, ids, errorMessage];
+}
