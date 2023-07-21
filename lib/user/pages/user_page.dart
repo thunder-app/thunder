156fe21563648da3810540c6b5e71890104f5ef8
Micah Morrison <micahmorrison95@gmail.com>
2023-07-21 00:51:06 -0400

    Accounts settings for blocked entities

--- lib/user/pages/user_page.dart
+++ lib/user/pages/user_page.dart
@@ -9,6 +9,7 @@ import 'package:thunder/core/auth/bloc/auth_bloc.dart';
 import 'package:thunder/user/pages/user_page_success.dart';
 import 'package:thunder/shared/error_message.dart';
 import 'package:thunder/user/bloc/user_bloc.dart';
+import 'package:thunder/user/pages/user_settings_page.dart';
 
 class UserPage extends StatefulWidget {
   final int? userId;
@@ -66,9 +67,26 @@ class _UserPageState extends State<UserPage> {
               )
             : null,
         actions: [
+          if (widget.userId != null && widget.isAccountUser)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4.0, 4.0, 0, 4.0),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => UserSettingsPage(widget.userId),
+                    ),
+                  );
+                },
+                icon: const Icon(
+                  Icons.settings_rounded,
+                  semanticLabel: 'Account Settings',
+                ),
+              ),
+            ),
           if (widget.isAccountUser)
             Padding(
-              padding: const EdgeInsets.all(4.0),
+              padding: const EdgeInsets.fromLTRB(0, 4.0, 4.00, 4.0),
               child: IconButton(
                 onPressed: () => showProfileModalSheet(context),
                 icon: const Icon(
