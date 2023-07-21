99c3ea4b8f8ad26aa9de06cf7b8eae403bac0fde
CTalvio <ctalvio@gmail.com>
2023-07-21 06:24:47 +0300

    FAB rework commit one

--- lib/community/widgets/post_card_list.dart
+++ lib/community/widgets/post_card_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math' as math;
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lemmy_api_client/v3.dart';
@@ -46,11 +47,28 @@ class PostCardList extends StatefulWidget {
   State<PostCardList> createState() => _PostCardListState();
 }
 
-class _PostCardListState extends State<PostCardList> {
+class _PostCardListState extends State<PostCardList> with TickerProviderStateMixin {
   final _scrollController = ScrollController(initialScrollOffset: 0);
   bool _showReturnToTopButton = false;
   int _previousScrollId = 0;
   bool disableFabs = false;
+
+  bool showFABMenu = false;
+  bool showRead = true;
+
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 200),
+    vsync: this,
+  );
+
+  // Animation for comment collapse
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(0.0, 2.0),
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  ));
 
   @override
   void initState() {
@@ -161,31 +179,84 @@ class _PostCardListState extends State<PostCardList> {
                   }
                 } else {
                   PostViewMedia postViewMedia = widget.postViews![(widget.communityId != null || widget.communityName != null) ? index - 1 : index];
-                  return PostCard(
-                    postViewMedia: postViewMedia,
-                    showInstanceName: widget.communityId == null,
-                    onVoteAction: (VoteType voteType) => widget.onVoteAction(postViewMedia.postView.post.id, voteType),
-                    onSaveAction: (bool saved) => widget.onSaveAction(postViewMedia.postView.post.id, saved),
-                    onToggleReadAction: (bool read) => widget.onToggleReadAction(postViewMedia.postView.post.id, read),
+                  return AnimatedSwitcher(
+                    duration: Duration(milliseconds: postViewMedia.postView.read ? 500 : 0),
+                    transitionBuilder: (Widget child, Animation<double> animation) {
+                      return FadeTransition(
+                        opacity: Tween<double>(begin: 0.0, end: 1.0).animate(
+                          CurvedAnimation(
+                            parent: animation,
+                            curve: const Interval(0.0, 0.5),
+                          ),
+                        ),
+                        child: SizeTransition(
+                          sizeFactor: Tween<double>(begin: 0.0, end: 1.0).animate(
+                            CurvedAnimation(
+                              parent: animation,
+                              curve: const Interval(0.5, 1.0),
+                            ),
+                          ),
+                          child: child),
+                      );
+                    },
+                    child: !postViewMedia.postView.read || showRead ? PostCard(
+                      postViewMedia: postViewMedia,
+                      showInstanceName: widget.communityId == null,
+                      onVoteAction: (VoteType voteType) => widget.onVoteAction(postViewMedia.postView.post.id, voteType),
+                      onSaveAction: (bool saved) => widget.onSaveAction(postViewMedia.postView.post.id, saved),
+                      onToggleReadAction: (bool read) => widget.onToggleReadAction(postViewMedia.postView.post.id, read),
+                    ) : Container(),
                   );
                 }
               },
             ),
-            if (!state.disableFeedFab && _showReturnToTopButton)
-              Positioned(
-                bottom: 16,
-                left: 20,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    scrollToTop();
-                  },
-                  child: const Icon(Icons.arrow_upward),
+            if (!state.disableFeedFab /*&& _showReturnToTopButton*/)
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: GestureFab(
+                  distance: 60,
+                  children: [
+                    ActionButton(
+                      onPressed: () {dismissRead();},
+                      icon: const Icon(Icons.clear_all_rounded),
+                    ),
+                    ActionButton(
+                      onPressed: null,
+                      icon: const Icon(Icons.insert_photo),
+                    ),
+                    ActionButton(
+                      onPressed: null,
+                      icon: const Icon(Icons.videocam),
+                    ),
+                    ActionButton(
+                      onPressed: null,
+                      icon: const Icon(Icons.videocam),
+                    ),
+                    ActionButton(
+                      onPressed: null,
+                      icon: const Icon(Icons.videocam),
+                    ),
+                  ],
                 ),
               ),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> dismissRead() async {
+    if (widget.postViews != null) {
+      setState(() {
+        showRead = false;
+      });
+      await Future.delayed(const Duration(milliseconds: 1000));
+      setState(() {
+        widget.postViews!.removeWhere( (e) => e.postView.read);
+        showRead = true;
+      });
+      widget.onScrollEndReached();
+    }
   }
 
   void scrollToTop() {
@@ -196,3 +267,206 @@ class _PostCardListState extends State<PostCardList> {
     );
   }
 }
+
+@immutable
+class GestureFab extends StatefulWidget {
+  const GestureFab({
+    super.key,
+    this.initialOpen,
+    required this.distance,
+    required this.children,
+  });
+
+  final bool? initialOpen;
+  final double distance;
+  final List<Widget> children;
+
+  @override
+  State<GestureFab> createState() => _GestureFabState();
+}
+
+class _GestureFabState extends State<GestureFab> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _expandAnimation;
+  bool _open = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _open = widget.initialOpen ?? false;
+    _controller = AnimationController(
+      value: _open ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+    _expandAnimation = CurvedAnimation(
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.easeOutQuad,
+      parent: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggle() {
+    setState(() {
+      _open = !_open;
+      if (_open) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        clipBehavior: Clip.none,
+        children: [
+          _buildTapToCloseFab(),
+          ..._buildExpandingActionButtons(),
+          _buildTapToOpenFab(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTapToCloseFab() {
+    return SizedBox(
+      width: 56,
+      height: 56,
+      child: Center(
+        child: Material(
+          shape: const CircleBorder(),
+          clipBehavior: Clip.antiAlias,
+          elevation: 4,
+          child: InkWell(
+            onTap: _toggle,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Icon(
+                Icons.close,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildExpandingActionButtons() {
+    final children = <Widget>[];
+    final count = widget.children.length;
+    for (var i = 0, distance = widget.distance;
+    i < count;
+    i++, distance += widget.distance) {
+      children.add(
+        _ExpandingActionButton(
+          maxDistance: distance,
+          progress: _expandAnimation,
+          child: widget.children[i],
+        ),
+      );
+    }
+    return children;
+  }
+
+  Widget _buildTapToOpenFab() {
+    return IgnorePointer(
+      ignoring: _open,
+      child: AnimatedContainer(
+        transformAlignment: Alignment.center,
+        transform: Matrix4.diagonal3Values(
+          _open ? 0.7 : 1.0,
+          _open ? 0.7 : 1.0,
+          1.0,
+        ),
+        duration: const Duration(milliseconds: 250),
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+        child: AnimatedOpacity(
+          opacity: _open ? 0.0 : 1.0,
+          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
+          duration: const Duration(milliseconds: 250),
+          child: FloatingActionButton(
+            onPressed: _toggle,
+            child: const Icon(Icons.create),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class ActionButton extends StatelessWidget {
+  const ActionButton({
+    super.key,
+    this.onPressed,
+    required this.icon,
+  });
+
+  final VoidCallback? onPressed;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Material(
+      borderRadius: const BorderRadius.all(Radius.circular(8)),
+      clipBehavior: Clip.antiAlias,
+      color: theme.colorScheme.primaryContainer,
+      elevation: 4,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: icon,
+        color: theme.colorScheme.onBackground,
+      ),
+    );
+  }
+}
+
+@immutable
+class _ExpandingActionButton extends StatelessWidget {
+  const _ExpandingActionButton({
+
+    required this.maxDistance,
+    required this.progress,
+    required this.child,
+  });
+
+  final double maxDistance;
+  final Animation<double> progress;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: progress,
+      builder: (context, child) {
+        final offset = Offset.fromDirection(
+          90 * (math.pi / 180.0),
+          progress.value * maxDistance,
+        );
+        return Positioned(
+          right: 8.0 + offset.dx,
+          bottom: 10.0 + offset.dy,
+          child: child!,
+        );
+      },
+      child: FadeTransition(
+        opacity: progress,
+        child: child,
+      ),
+    );
+  }
+}
+
+
