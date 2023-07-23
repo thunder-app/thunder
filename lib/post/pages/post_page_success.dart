c24aeded33b2820ebaee73b3368c9e601b0b890e
ajsosa <armandojosuesosa@gmail.com>
2023-07-23 01:33:41 -0500

    Fixed formatting

--- lib/post/pages/post_page_success.dart
+++ lib/post/pages/post_page_success.dart
@@ -73,8 +73,7 @@ class _PostPageSuccessState extends State<PostPageSuccess> {
             postViewMedia: widget.postView,
             comments: widget.comments,
             hasReachedCommentEnd: widget.hasReachedCommentEnd,
-            onVoteAction: (int commentId, VoteType voteType) =>
-                context.read<PostBloc>().add(VoteCommentEvent(commentId: commentId, score: voteType)),
+            onVoteAction: (int commentId, VoteType voteType) => context.read<PostBloc>().add(VoteCommentEvent(commentId: commentId, score: voteType)),
             onSaveAction: (int commentId, bool save) => context.read<PostBloc>().add(SaveCommentEvent(commentId: commentId, save: save)),
             onDeleteAction: (int commentId, bool deleted) => context.read<PostBloc>().add(DeleteCommentEvent(deleted: deleted, commentId: commentId)),
           ),
