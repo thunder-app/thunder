85087a9904d567b1d4e2d5c1de52802f64e75d8c
Micah Morrison <micahmorrison95@gmail.com>
2023-07-25 15:20:32 -0400

    Fix version parsing

--- lib/core/update/check_github_update.dart
+++ lib/core/update/check_github_update.dart
@@ -4,8 +4,9 @@ import 'package:http/http.dart' as http;
 import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:thunder/core/models/version.dart';
+import 'package:version/version.dart' as version_parser;
 
-Future<String?> getCurrentVersion() async {
+Future<String> getCurrentVersion() async {
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
   String version = packageInfo.version;
   String build = packageInfo.buildNumber;
@@ -17,7 +18,8 @@ Future<Version> fetchVersion() async {
   const url = 'https://api.github.com/repos/hjiangsu/thunder/releases/latest';
 
   try {
-    String? currentVersion = await getCurrentVersion();
+    String currentVersion = await getCurrentVersion();
+    version_parser.Version currentVersionParsed = version_parser.Version.parse(_trimV(currentVersion));
 
     final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 3));
 
@@ -25,7 +27,9 @@ Future<Version> fetchVersion() async {
       final release = json.decode(response.body);
       String latestVersion = release['tag_name'];
 
-      if (currentVersion != null && currentVersion.compareTo(latestVersion) < 0) {
+      version_parser.Version latestVersionParsed = version_parser.Version.parse(_trimV(latestVersion));
+
+      if (latestVersionParsed > currentVersionParsed) {
         return Version(version: currentVersion, latestVersion: latestVersion, hasUpdate: true);
       } else {
         return Version(version: 'N/A', latestVersion: latestVersion, hasUpdate: false);
@@ -36,4 +40,9 @@ Future<Version> fetchVersion() async {
   } catch (e) {
     return Version(version: 'N/A', latestVersion: 'N/A', hasUpdate: false);
   }
+}
+
+String _trimV(String version) {
+  if (version.startsWith('v')) return version.substring(1);
+  return version;
 }
