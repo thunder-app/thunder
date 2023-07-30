2ab7efa8e6bc38a5177ef815169ff9266f0f7fc5
CTalvio <ctalvio@gmail.com>
2023-07-30 19:49:55 +0300

    fix merge mistake

--- lib/settings/widgets/toggle_option.dart
+++ lib/settings/widgets/toggle_option.dart
@@ -36,24 +36,27 @@ class ToggleOption extends StatelessWidget {
             if (iconEnabled != null && iconEnabled != null) Icon(value ? iconEnabled : iconDisabled),
             if (iconEnabled != null && iconEnabled != null) const SizedBox(width: 8.0),
             Column(
-            ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 140),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(description, style: theme.textTheme.bodyMedium),
-                  if (subtitle != null) Text(subtitle!, style: theme.textTheme.bodySmall?.copyWith(color: theme.textTheme.bodySmall?.color?.withOpacity(0.8))),
-                ],
-              ),
+            children: [
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 140),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(description, style: theme.textTheme.bodyMedium),
+                    if (subtitle != null) Text(subtitle!, style: theme.textTheme.bodySmall?.copyWith(color: theme.textTheme.bodySmall?.color?.withOpacity(0.8))),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Switch(
+              value: value,
+              onChanged: (bool value) {
+                HapticFeedback.lightImpact();
+                onToggle(value);
+              },
             ),
           ],
-        ),
-        Switch(
-          value: value,
-          onChanged: (bool value) {
-            HapticFeedback.lightImpact();
-            onToggle(value);
-          },
         ),
       ],
     );
