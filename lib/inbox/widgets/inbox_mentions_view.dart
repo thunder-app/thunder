7a804e573d550915567fc3dab81d7733d1afb514
Micah Morrison <micahmorrison95@gmail.com>
2023-08-12 00:16:23 -0400

    Add SwipeablePageRoute dead zone to not interfere with Android back gesture

--- lib/inbox/widgets/inbox_mentions_view.dart
+++ lib/inbox/widgets/inbox_mentions_view.dart
@@ -45,6 +45,7 @@ class InboxMentionsView extends StatelessWidget {
               // To to specific post for now, in the future, will be best to scroll to the position of the comment
               await Navigator.of(context).push(
                 SwipeablePageRoute(
+                  backGestureDetectionStartOffset: 45,
                   canOnlySwipeFromEdge: disableFullPageSwipe(isUserLoggedIn: authBloc.state.isLoggedIn, state: thunderBloc.state, isPostPage: true),
                   builder: (context) => MultiBlocProvider(
                     providers: [
