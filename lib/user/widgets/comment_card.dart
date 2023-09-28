29561f712c340c3eb0237c8581fe0c38be56ce4d
Micah Morrison <micahmorrison95@gmail.com>
2023-09-27 23:32:57 -0400

    Fix accessibility semantics for replies/saved

--- lib/user/widgets/comment_card.dart
+++ lib/user/widgets/comment_card.dart
@@ -15,6 +15,7 @@ import 'package:thunder/utils/date_time.dart';
 import 'package:thunder/utils/instance.dart';
 import 'package:thunder/utils/navigate_community.dart';
 import 'package:thunder/utils/swipe.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../utils/numbers.dart';
 
@@ -81,7 +82,7 @@ class CommentCard extends StatelessWidget {
                   const SizedBox(width: 2.0),
                   Text(
                     formatNumberToK(upvotes),
-                    semanticsLabel: '${formatNumberToK(upvotes)} upvotes',
+                    semanticsLabel: AppLocalizations.of(context)!.xUpvotes(formatNumberToK(upvotes)),
                     textScaleFactor: MediaQuery.of(context).textScaleFactor * state.metadataFontSizeScale.textScaleFactor,
                     style: theme.textTheme.bodyMedium?.copyWith(
                       color: theme.colorScheme.onBackground,
@@ -97,7 +98,7 @@ class CommentCard extends StatelessWidget {
                   if (downvotes != 0)
                     Text(
                       formatNumberToK(downvotes),
-                      semanticsLabel: '${formatNumberToK(downvotes)} downvotes',
+                      semanticsLabel: AppLocalizations.of(context)!.xDownvotes(formatNumberToK(downvotes)),
                       textScaleFactor: MediaQuery.of(context).textScaleFactor * state.metadataFontSizeScale.textScaleFactor,
                       style: theme.textTheme.bodyMedium?.copyWith(
                         color: downvotes != 0 ? theme.colorScheme.onBackground : Colors.transparent,
