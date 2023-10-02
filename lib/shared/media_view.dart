c6a616b9422ce98e7d5ea0f0a7fa57c7c207b3d0
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-10-02 09:43:16 -0700

    fixed issues where media/link previews would not work when on feed or post

--- lib/shared/media_view.dart
+++ lib/shared/media_view.dart
@@ -164,13 +164,18 @@ class _MediaViewState extends State<MediaView> with SingleTickerProviderStateMix
                 onTap: () {
                   if (widget.isUserLoggedIn && widget.markPostReadOnMediaView) {
                     int postId = widget.postView!.postView.post.id;
+
+                    // Mark post as read when on the feed page
                     try {
                       FeedBloc feedBloc = BlocProvider.of<FeedBloc>(context);
                       feedBloc.add(FeedItemActionedEvent(postAction: PostAction.read, postId: postId, value: true));
-                    } catch (e) {
+                    } catch (e) {}
+
+                    // Mark post as read when on the user page
+                    try {
                       UserBloc userBloc = BlocProvider.of<UserBloc>(context);
-                      userBloc.add(MarkUserPostAsReadEvent(postId: postId, read: true));
-                    }
+                      userBloc.add(MarkUserPostAsReadEvent(postId: postId!, read: true));
+                    } catch (e) {}
                   }
                   Navigator.of(context).push(
                     PageRouteBuilder(
