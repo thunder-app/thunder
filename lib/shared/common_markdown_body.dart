dc69442836648098a9600cf3d16e301ec1525fa2
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-10-30 13:53:48 -0700

    initial implementation for supporting spoiler syntax on markdown

--- lib/shared/common_markdown_body.dart
+++ lib/shared/common_markdown_body.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+import 'package:markdown/markdown.dart' as md;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:lemmy_api_client/v3.dart';
+
 import 'package:thunder/account/models/account.dart';
 import 'package:thunder/core/auth/helpers/fetch_account.dart';
 import 'package:thunder/core/enums/font_scale.dart';
@@ -37,8 +40,15 @@ class CommonMarkdownBody extends StatelessWidget {
 
     bool openInExternalBrowser = state.openInExternalBrowser;
 
+    // Custom extension set
+    md.ExtensionSet customExtensionSet = md.ExtensionSet.gitHubFlavored;
+    customExtensionSet = md.ExtensionSet(List.from(customExtensionSet.blockSyntaxes)..add(SpoilerBlockSyntax()), List.from(customExtensionSet.inlineSyntaxes));
+
     return MarkdownBody(
-      // TODO We need spoiler support here
+      extensionSet: customExtensionSet,
+      builders: {
+        'spoiler': SpoilerElementBuilder(),
+      },
       data: body,
       imageBuilder: (uri, title, alt) {
         return Padding(
@@ -147,6 +157,125 @@ class CommonMarkdownBody extends StatelessWidget {
         blockquoteDecoration: const BoxDecoration(
           color: Colors.transparent,
           border: Border(left: BorderSide(color: Colors.grey, width: 4)),
+        ),
+      ),
+    );
+  }
+}
+
+/// A Markdown Extension to handle spoiler tags on Lemmy. This extends the [md.BlockSyntax]
+/// to allow for multi-line parsing of text for a given spoiler tag.
+///
+/// It parses the following syntax for a spoiler:
+///
+/// ```
+/// ::: spoiler spoiler_title
+/// spoiler_body
+/// :::
+/// ```
+class SpoilerBlockSyntax extends md.BlockSyntax {
+  /// The pattern to match the end of a spoiler
+  static final RegExp _spoilerBlockEnd = RegExp(r'^(?:::)$');
+
+  /// The pattern to match the beginning of a spoiler
+  @override
+  RegExp get pattern => RegExp(r'^:::\s+spoiler\s+(.+)\s*$');
+
+  @override
+  bool canParse(md.BlockParser parser) {
+    return pattern.hasMatch(parser.current.content);
+  }
+
+  /// Parses the block of text for the given spoiler. This will fetch the title and the body of the spoiler.
+  @override
+  md.Node parse(md.BlockParser parser) {
+    final Match? match = pattern.firstMatch(parser.current.content);
+    final String? title = match?.group(1)?.trim();
+
+    parser.advance(); // Move to the next line
+
+    final List<String> body = [];
+
+    // Accumulate lines of the body until the closing :::
+    while (!parser.isDone) {
+      if (_spoilerBlockEnd.hasMatch(parser.current.content)) {
+        parser.advance();
+        break;
+      }
+      body.add(parser.current.content);
+      parser.advance();
+    }
+
+    // Create a custom Node which will be used to render the spoiler in [SpoilerElementBuilder]
+    final md.Node spoiler = md.Element('p', [
+      /// This is a workaround to allow us to parse the spoiler title and body within the [SpoilerElementBuilder]
+      ///
+      /// If the title and body are passed as separate elements into the [spoiler] tag, it causes
+      /// the resulting [SpoilerWidget] to always show the second element. To work around this, the title and
+      /// body are placed together into a single node, separated by a ::: to distinguish the sections.
+      md.Element('spoiler', [
+        md.UnparsedContent('${title ?? 'spoiler'}:::${body.join('\n')}'),
+      ]),
+    ]);
+
+    return spoiler;
+  }
+}
+
+/// Creates a [MarkdownElementBuilder] that renders the custom spoiler tag defined in [SpoilerSyntax].
+///
+/// This breaks down the combined title/body and creates the resulting [SpoilerWidget]
+class SpoilerElementBuilder extends MarkdownElementBuilder {
+  @override
+  Widget? visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    String rawText = element.textContent;
+    List<String> parts = rawText.split(':::');
+
+    if (parts.length != 2) {
+      // An invalid spoiler format
+      return Container();
+    }
+
+    String? title = parts[0].trim();
+    String? body = parts[1].trim();
+    return SpoilerWidget(title: title, body: body);
+  }
+}
+
+/// Creates a widget that toggles the visibility of the given [body].
+/// It displays the [title] of the spoiler by default unless tapped.
+///
+/// If no [title] is given, it will display "spoiler" as the default text.
+class SpoilerWidget extends StatefulWidget {
+  final String? title;
+  final String? body;
+
+  const SpoilerWidget({super.key, this.title, this.body});
+
+  @override
+  State<SpoilerWidget> createState() => _SpoilerWidgetState();
+}
+
+class _SpoilerWidgetState extends State<SpoilerWidget> {
+  bool isShown = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    if (isShown) {
+      return GestureDetector(
+        child: CommonMarkdownBody(body: widget.body ?? ''),
+        onTap: () => setState(() => isShown = false),
+      );
+    }
+
+    return RichText(
+      text: TextSpan(
+        text: widget.title ?? 'spoiler',
+        recognizer: TapGestureRecognizer()..onTap = () => setState(() => isShown = true),
+        style: theme.textTheme.bodyMedium?.copyWith(
+          color: theme.colorScheme.secondary,
         ),
       ),
     );
