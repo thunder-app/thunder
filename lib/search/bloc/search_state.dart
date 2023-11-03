cd59e7a00a8a4566e22b30fb94a6f5f0a6eb41b6
Micah Morrison <micahmorrison95@gmail.com>
2023-11-03 11:28:02 -0400

    Add comments as supported search type (#861)

--- lib/search/bloc/search_state.dart
+++ lib/search/bloc/search_state.dart
@@ -1,6 +1,6 @@
 part of 'search_bloc.dart';
 
-enum SearchStatus { initial, trending, loading, refreshing, success, empty, failure, done }
+enum SearchStatus { initial, trending, loading, refreshing, success, empty, failure, done, performingCommentAction }
 
 class SearchState extends Equatable {
   SearchState({
@@ -8,6 +8,7 @@ class SearchState extends Equatable {
     this.communities,
     this.trendingCommunities,
     this.users,
+    this.comments,
     this.errorMessage,
     this.page = 1,
     this.sortType,
@@ -18,6 +19,7 @@ class SearchState extends Equatable {
   List<CommunityView>? communities;
   List<CommunityView>? trendingCommunities;
   List<PersonView>? users;
+  List<CommentView>? comments;
 
   final String? errorMessage;
 
@@ -31,6 +33,7 @@ class SearchState extends Equatable {
     List<CommunityView>? communities,
     List<CommunityView>? trendingCommunities,
     List<PersonView>? users,
+    List<CommentView>? comments,
     String? errorMessage,
     int? page,
     SortType? sortType,
@@ -41,6 +44,7 @@ class SearchState extends Equatable {
       communities: communities ?? this.communities,
       trendingCommunities: trendingCommunities ?? this.trendingCommunities,
       users: users ?? this.users,
+      comments: comments ?? this.comments,
       errorMessage: errorMessage,
       page: page ?? this.page,
       sortType: sortType ?? this.sortType,
