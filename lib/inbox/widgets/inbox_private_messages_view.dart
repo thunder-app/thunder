655afa9d42c753ce19b1bd43bd796559c222ef85
ggichure <g.gichure@outlook.com>
2023-11-07 13:54:13 +0300

    init: group inbox messages by recipient id

--- lib/inbox/widgets/inbox_private_messages_view.dart
+++ lib/inbox/widgets/inbox_private_messages_view.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lemmy_api_client/v3.dart';
+import 'package:thunder/account/models/account.dart';
+import 'package:thunder/core/auth/bloc/auth_bloc.dart';
 
 import 'package:thunder/shared/common_markdown_body.dart';
+import 'package:thunder/shared/user_avatar.dart';
 import 'package:thunder/utils/date_time.dart';
+import 'package:thunder/utils/instance.dart';
+import 'package:thunder/utils/navigate_user.dart';
 
 class InboxPrivateMessagesView extends StatelessWidget {
   final List<PrivateMessageView> privateMessages;
@@ -16,6 +22,7 @@ class InboxPrivateMessagesView extends StatelessWidget {
     if (privateMessages.isEmpty) {
       return Align(alignment: Alignment.topCenter, heightFactor: (MediaQuery.of(context).size.height / 27), child: const Text('No messages'));
     }
+    final Account? account = context.read<AuthBloc>().state.account;
 
     return ListView.builder(
       shrinkWrap: true,
@@ -23,40 +30,67 @@ class InboxPrivateMessagesView extends StatelessWidget {
       itemCount: privateMessages.length,
       itemBuilder: (context, index) {
         return Card(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            child: GestureDetector(
+              onTap: () {
+                navigateToUserPage(
+                  context,
+                  userId: (account?.userId == privateMessages[index].recipient.id) ? privateMessages[index].creator.id : privateMessages[index].recipient.id,
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
+                    Stack(
                       children: [
-                        Text(
-                          privateMessages[index].creator.name,
-                          style: theme.textTheme.titleSmall?.copyWith(color: Colors.greenAccent),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: UserAvatar(
+                            person: (account?.userId == privateMessages[index].recipient.id) ? privateMessages[index].creator : privateMessages[index].recipient,
+                            radius: 24.0,
+                          ),
                         ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Icon(Icons.arrow_forward_rounded, size: 14),
-                        ),
-                        Text(
-                          privateMessages[index].recipient.name,
-                          style: theme.textTheme.titleSmall?.copyWith(color: Colors.greenAccent),
-                        ),
+                        if (privateMessages[index].privateMessage.read == false)
+                          //const  Badge()
+                          Positioned(
+                            bottom: 0,
+                            left: -4,
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Icon(
+                                Icons.fiber_manual_record,
+                                color: Theme.of(context).primaryColor.withOpacity(.7),
+                              ),
+                            ),
+                          )
                       ],
                     ),
-                    Text(formatTimeToString(dateTime: privateMessages[index].privateMessage.published.toIso8601String()))
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            (account?.userId == privateMessages[index].recipient.id)
+                                ? '${privateMessages[index].creator.displayName ?? privateMessages[index].creator.name}@${fetchInstanceNameFromUrl(privateMessages[index].creator.actorId) ?? ""}'
+                                : '${privateMessages[index].recipient.displayName ?? privateMessages[index].recipient.name}@${fetchInstanceNameFromUrl(privateMessages[index].recipient.actorId) ?? ""}',
+                            style: theme.textTheme.titleSmall?.copyWith(color: Colors.greenAccent, fontWeight: FontWeight.bold),
+                          ),
+                          CommonMarkdownBody(body: privateMessages[index].privateMessage.content),
+                        ],
+                      ),
+                    ),
+                    Text(formatTimeToString(dateTime: privateMessages[index].privateMessage.published.toIso8601String())),
                   ],
                 ),
-                const SizedBox(height: 10),
-                CommonMarkdownBody(body: privateMessages[index].privateMessage.content),
-              ],
-            ),
-          ),
-        );
+              ),
+            ));
       },
     );
   }
