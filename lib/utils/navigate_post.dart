c8616b1f561633374eb835f17b3c4b361b2d4a6a
Hamlet Jiang Su <30667958+hjiangsu@users.noreply.github.com>
2023-11-09 10:16:16 -0800

    Added compatibility with new Lemmy API changes 0.19.0-alpha.16 (#881)
    * added compatability with new lemmy api changes 0.19.0-alpha.16
    
    * simplified markPostAsRead

--- lib/utils/navigate_post.dart
+++ lib/utils/navigate_post.dart
@@ -77,7 +77,8 @@ Future<void> navigateToPost(BuildContext context, {PostViewMedia? postViewMedia,
               try {
                 feedBloc = context.read<FeedBloc>();
               } catch (e) {}
-              feedBloc?.add(FeedItemUpdatedEvent(postViewMedia: postViewMedia));
+              // Manually marking the read attribute as true when navigating to post since there is a case where the API call to mark the post as read from the feed page is not completed in time
+              feedBloc?.add(FeedItemUpdatedEvent(postViewMedia: PostViewMedia(postView: postViewMedia.postView.copyWith(read: true), media: postViewMedia.media)));
             },
           ),
         );
