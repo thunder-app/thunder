d7a13eeed3aea0fd22aaea7a4df693553dceeb7c
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-11-21 09:16:30 -0800

    Add ability to open links in thunder from safari extension

---
+++ ios/Safari Extension/Resources/content.js
@@ -0,0 +1,99 @@
+let instances = [
+    "aussie.zone",
+    "bakchodi.org",
+    "beehaw.org",
+    "burggit.moe",
+    "dataterm.digital",
+    "delraymisfitsboard.com",
+    "discuss.tchncs.de",
+    "exploding-heads.com",
+    "feddit.ch",
+    "feddit.cl",
+    "feddit.de",
+    "feddit.dk",
+    "feddit.it",
+    "feddit.nl",
+    "feddit.nu",
+    "feddit.uk",
+    "geddit.social",
+    "hexbear.net",
+    "infosec.pub",
+    "iusearchlinux.fyi",
+    "jlai.lu",
+    "lemdro.id",
+    "lemm.ee",
+    "lemmings.world",
+    "lemmus.org",
+    "lemmy.blahaj.zone",
+    "lemmy.ca",
+    "lemmy.dbzer0.com",
+    "lemmy.eco.br",
+    "lemmy.fmhy.ml",
+    "lemmy.kya.moe",
+    "lemmy.ml",
+    "lemmy.nz",
+    "lemmy.one",
+    "lemmy.sdf.org",
+    "lemmy.today",
+    "lemmy.world",
+    "lemmy.zip",
+    "lemmygrad.ml",
+    "lemmynsfw.com",
+    "mander.xyz",
+    "midwest.social",
+    "monero.town",
+    "monyet.cc",
+    "pawb.social",
+    "programming.dev",
+    "reddthat.com",
+    "sh.itjust.works",
+    "slrpnk.net",
+    "social.fossware.space",
+    "sopuli.xyz",
+    "startrek.website",
+    "szmer.info",
+    "ttrpg.network",
+    "vlemmy.net",
+    "waveform.social",
+    "www.hexbear.net",
+    "yiffit.net",
+];
+
+const observeUrlChange = () => {
+    let oldHref = document.location.href;
+    
+    const body = document.querySelector("body");
+    const observer = new MutationObserver((mutations) => {
+        if (oldHref !== document.location.href) {
+            oldHref = document.location.href;
+            openInThunder();
+        }
+    });
+    
+    observer.observe(body, { childList: true, subtree: true });
+};
+
+
+function isLemmyInstance(arr) {
+    const currentHost = new URL(document.location.href).host;
+    
+    for (let i = 0; i < instances.length; i++) {
+        if (currentHost.includes(instances[i])) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+function openInThunder() {
+    const shouldOpen = isLemmyInstance();
+    if (!shouldOpen) return;
+    
+    let url = new URL(document.location.href);
+    url.protocol = "thunder:";
+    window.location.href = url;
+}
+
+openInThunder();
+window.onload = observeUrlChange;
