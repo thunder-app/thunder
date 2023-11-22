225183ea70362233ec10ffde685aae6a734508d3
Hamlet Jiang Su <hamlet.jiangsu@outlook.com>
2023-11-21 17:05:25 -0800

    renamed safari extension

--- ios/Open In Thunder/Resources/content.js
+++ ios/Open In Thunder/Resources/content.js
@@ -97,3 +97,4 @@ function openInThunder() {
 
 openInThunder();
 window.onload = observeUrlChange;
+
--- ios/Open In Thunder/SafariWebExtensionHandler.swift
+++ ios/Open In Thunder/SafariWebExtensionHandler.swift
@@ -15,3 +15,4 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
     }
 
 }
+
