03a54bd06bb970bb7ab6c891baef2d5322338ea3
Garrett Bischof <bischofgarrett@gmail.com>
2023-12-02 11:41:57 -0500

    add scripts

--- scripts/docker-build-web.sh
+++ scripts/docker-build-web.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 D=$(date +'%Y%m%d.%H%M%S%3N')
 
 cd "${SCRIPT_DIR}/.."
-# Build the apk inside the image
 docker build \
-    -t thunder-builder \
+    -t thunder-web \
     -f ./docker/DockerfileWeb .
