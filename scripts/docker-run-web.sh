79211d2106bd2a0e2f7bd28d25bed5ed5c2794ef
Garrett Bischof <bischofgarrett@gmail.com>
2023-12-03 01:38:24 -0500

    add comment

--- scripts/docker-run-web.sh
+++ scripts/docker-run-web.sh
@@ -2,6 +2,8 @@
 
 # This script runs thunder-web docker image after it is built using the docker-build-web.sh script.
 
+# Go to http://localhost:8080/ to view the app.
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "${SCRIPT_DIR}/.."
