d570badd896c97ed6654618f7f1298a1494d604e
Micah Morrison <micahmorrison95@gmail.com>
2023-12-26 11:24:20 -0500

    Improve number formatting

--- lib/utils/numbers.dart
+++ lib/utils/numbers.dart
@@ -1,7 +1,8 @@
-String formatNumberToK(int number) {
-  if (number.abs() > 999) {
-    return '${(number.sign * number.abs() / 1000).toStringAsFixed(1)}K';
-  } else {
-    return (number.sign * number.abs()).toString();
-  }
-}
+import 'package:intl/intl.dart';
+
+final NumberFormat _compactFormatter = NumberFormat.compact();
+final NumberFormat _longFormatter = NumberFormat.decimalPatternDigits(decimalDigits: 0);
+
+String formatNumberToK(int number) => _compactFormatter.format(number);
+
+String formatLongNumber(int number) => _longFormatter.format(number);
