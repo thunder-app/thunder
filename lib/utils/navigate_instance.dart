80f5eb1bff1bd9abb2ae2e5c6ceb9574306f0664
Micah Morrison <micahmorrison95@gmail.com>
2024-01-22 15:29:01 -0500

    Handle errors with navigation to non-instances (#1076)

--- lib/utils/navigate_instance.dart
+++ lib/utils/navigate_instance.dart
@@ -3,14 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lemmy_api_client/v3.dart';
 import 'package:swipeable_page_route/swipeable_page_route.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:thunder/account/models/account.dart';
 import 'package:thunder/core/auth/helpers/fetch_account.dart';
 import 'package:thunder/core/singletons/lemmy_client.dart';
 import 'package:thunder/instance/instance_page.dart';
+import 'package:thunder/shared/snackbar.dart';
 import 'package:thunder/thunder/bloc/thunder_bloc.dart';
 
 Future<void> navigateToInstancePage(BuildContext context, {required String instanceHost, required int? instanceId}) async {
+  final AppLocalizations l10n = AppLocalizations.of(context)!;
+
   ThunderBloc thunderBloc = context.read<ThunderBloc>();
 
   final bool reduceAnimations = thunderBloc.state.reduceAnimations;
@@ -27,23 +31,27 @@ Future<void> navigateToInstancePage(BuildContext context, {required String insta
     isBlocked = localSite.myUser?.instanceBlocks?.any((i) => i.instance.domain == instanceHost) == true;
   } catch (e) {}
 
-  if (getSiteResponse?.siteView.site != null && context.mounted) {
-    Navigator.of(context).push(
-      SwipeablePageRoute(
-        transitionDuration: reduceAnimations ? const Duration(milliseconds: 100) : null,
-        backGestureDetectionWidth: 45,
-        canOnlySwipeFromEdge: !thunderBloc.state.enableFullScreenSwipeNavigationGesture,
-        builder: (context) => MultiBlocProvider(
-          providers: [
-            BlocProvider.value(value: thunderBloc),
-          ],
-          child: InstancePage(
-            getSiteResponse: getSiteResponse!,
-            isBlocked: isBlocked,
-            instanceId: instanceId,
+  if (context.mounted) {
+    if (getSiteResponse?.siteView.site != null) {
+      Navigator.of(context).push(
+        SwipeablePageRoute(
+          transitionDuration: reduceAnimations ? const Duration(milliseconds: 100) : null,
+          backGestureDetectionWidth: 45,
+          canOnlySwipeFromEdge: !thunderBloc.state.enableFullScreenSwipeNavigationGesture,
+          builder: (context) => MultiBlocProvider(
+            providers: [
+              BlocProvider.value(value: thunderBloc),
+            ],
+            child: InstancePage(
+              getSiteResponse: getSiteResponse!,
+              isBlocked: isBlocked,
+              instanceId: instanceId,
+            ),
           ),
         ),
-      ),
-    );
+      );
+    } else {
+      showSnackbar(context, l10n.unableToNavigateToInstance(instanceHost));
+    }
   }
 }
