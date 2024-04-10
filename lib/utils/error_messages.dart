4646022eb3d1e82490ded9ed9e80a3e8fa40e465
Micah Morrison <micahmorrison95@gmail.com>
2024-04-10 11:40:35 -0400

    Improve missing post message

--- lib/utils/error_messages.dart
+++ lib/utils/error_messages.dart
@@ -31,6 +31,7 @@ String? getErrorMessage(BuildContext context, String lemmyApiErrorCode, {String?
     "language_not_allowed" => l10n.languageNotAllowed,
     "couldnt_find_community" => additionalInfo != null ? l10n.unableToFindCommunityName(additionalInfo) : l10n.unableToFindCommunity,
     "couldnt_find_person" => additionalInfo != null ? l10n.unableToFindUserName(additionalInfo) : l10n.unableToFindUser,
+    "couldnt_find_post" => l10n.couldntFindPost,
     _ => lemmyApiErrorCode,
   };
 }
