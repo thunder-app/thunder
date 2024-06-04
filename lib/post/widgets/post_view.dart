768819324a39c2b7e00249b2f5df491e128c75be
Micah Morrison <micahmorrison95@gmail.com>
2024-06-04 15:55:41 -0400

    Display account birthdays in user chip (#1411)
    * Show account birthdays
    
    * Show account birthday/age on new line of tooltip
    
    Co-authored-by: Hamlet Jiang Su <30667958+hjiangsu@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Hamlet Jiang Su <30667958+hjiangsu@users.noreply.github.com>

--- lib/post/widgets/post_view.dart
+++ lib/post/widgets/post_view.dart
@@ -124,6 +124,7 @@ class _PostSubviewState extends State<PostSubview> with SingleTickerProviderStat
     if (postView.creatorIsModerator ?? false) userGroups.add(UserType.moderator);
     if (postView.creatorIsAdmin ?? false) userGroups.add(UserType.admin);
     if (postView.creator.id == authState.account?.userId) userGroups.add(UserType.self);
+    if (postView.creator.published.month == DateTime.now().month && postView.creator.published.day == DateTime.now().day) userGroups.add(UserType.birthday);
 
     return ExpandableNotifier(
       controller: expandableController,
@@ -229,11 +230,8 @@ class _PostSubviewState extends State<PostSubview> with SingleTickerProviderStat
                     crossAxisAlignment: WrapCrossAlignment.center,
                     children: [
                       UserChip(
-                        personId: postView.creator.id,
+                        person: postView.creator,
                         personAvatar: UserAvatar(person: postView.creator, radius: 10, thumbnailSize: 20, format: 'png'),
-                        personName: postView.creator.name,
-                        personDisplayName: postView.creator.displayName ?? postView.creator.name,
-                        personUrl: postView.creator.actorId,
                         userGroups: userGroups,
                         includeInstance: thunderState.postBodyShowCommunityInstance,
                       ),
