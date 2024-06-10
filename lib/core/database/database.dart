f563dbb3f1268dad6f20f99cf22b64def631a0c3
Micah Morrison <micahmorrison95@gmail.com>
2024-06-10 14:58:36 -0400

    Add ability to import/export database (#1432)

--- lib/core/database/database.dart
+++ lib/core/database/database.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/material.dart' hide Table;
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqlite3/sqlite3.dart';
@@ -37,4 +39,47 @@ LazyDatabase _openConnection() {
 
     return NativeDatabase.createInBackground(file);
   });
+}
+
+Future<String?> exportDatabase() async {
+  final Directory dbFolder = await getApplicationDocumentsDirectory();
+  final File file = File(join(dbFolder.path, 'thunder.sqlite'));
+
+  return await FlutterFileDialog.saveFile(
+    params: SaveFileDialogParams(
+      mimeTypesFilter: ['application/octet-stream'],
+      sourceFilePath: file.path,
+      fileName: 'thunder.sqlite',
+    ),
+  );
+}
+
+Future<bool> importDatabase() async {
+  final String? filePath = await FlutterFileDialog.pickFile(
+    params: const OpenFileDialogParams(
+      fileExtensionsFilter: ['sqlite'],
+    ),
+  );
+
+  if (filePath != null) {
+    final Directory dbFolder = await getApplicationDocumentsDirectory();
+    final File file = File(join(dbFolder.path, 'thunder.sqlite'));
+
+    try {
+      // Read the selected db file
+      final List<int> bytes = await File(filePath).readAsBytes();
+
+      // Write the file out the location we read it from
+      await file.writeAsBytes(bytes, flush: true);
+
+      // Since the db calls go straight to the file, we don't need to reload anything
+      return true;
+    } catch (e) {
+      debugPrint('Error importing sqlite db: $e');
+    }
+  } else {
+    debugPrint("Database import operation cancelled by user.");
+  }
+
+  return false;
 }
