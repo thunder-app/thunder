4d4c8cd517027fd08a983ea76943d90f41c29757
Garrett Bischof <bischofgarrett@gmail.com>
2024-10-30 19:25:28 -0400

    update database name

--- lib/core/database/connection/web.dart
+++ lib/core/database/connection/web.dart
@@ -12,7 +12,7 @@ Future<void> validateDatabaseSchema(GeneratedDatabase database) async {
 DatabaseConnection connectOnWeb() {
   return DatabaseConnection.delayed(Future(() async {
     final result = await WasmDatabase.open(
-      databaseName: 'my_app_db', // prefer to only use valid identifiers here
+      databaseName: 'thunder', // prefer to only use valid identifiers here
       sqlite3Uri: Uri.parse('sqlite3.wasm'),
       driftWorkerUri: Uri.parse('drift_worker.dart.js'),
     );
