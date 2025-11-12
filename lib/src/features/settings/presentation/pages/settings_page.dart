1bb46af6de797e9d140d220fcabaa6baa2fa65da
Micah Morrison <micahmorrison95@gmail.com>
2025-11-12 17:53:00 -0500

    Fix settings search elevation

--- lib/src/features/settings/presentation/pages/settings_page.dart
+++ lib/src/features/settings/presentation/pages/settings_page.dart
@@ -61,6 +61,7 @@ class _SettingsPageState extends State<SettingsPage> {
                 child: SearchAnchor.bar(
                   searchController: searchController,
                   barHintText: l10n.search,
+                  barElevation: const WidgetStatePropertyAll(0.0),
                   suggestionsBuilder: (BuildContext context, SearchController controller) {
                     final theme = Theme.of(context);
 
