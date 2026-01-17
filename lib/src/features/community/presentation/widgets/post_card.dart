3c2f986e3c7d02ec0b18e7d6155648f985573387
ludmila-lovelace <ada.bolshevik@gmail.com>
2026-01-17 13:38:04 +1100

    fix: remove unnecessary check for link urls

--- lib/src/features/community/presentation/widgets/post_card.dart
+++ lib/src/features/community/presentation/widgets/post_card.dart
@@ -181,7 +181,7 @@ class _PostCardState extends State<PostCard> {
     final feedType = context.select<FeedBloc, FeedType?>((bloc) => bloc.state.feedType);
     final postIsCompact = useCompactView ||
         (pinnedPostsUseCompactView && (widget.post.featuredLocal || (feedType == FeedType.community && widget.post.featuredCommunity))) ||
-        (linkPostsUseCompactView && widget.post.media.first.mediaType == MediaType.link && widget.post.media.first.originalUrl?.isNotEmpty == true);
+        (linkPostsUseCompactView && widget.post.media.first.mediaType == MediaType.link);
 
     // Determine which post card view to use based on the settings
     Widget child = postIsCompact
